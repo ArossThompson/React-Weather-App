ca32267cb745e16bd90104f4c223a6437d0113b2
Ross Thompson <rossathompson94@gmail.com>
2019-10-14 11:26:45 +0100

    refactored styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -22,10 +22,12 @@ class Search extends React.Component {
                         type="text" 
                         className="search-field__search"
                         value={this.state.term}
-                        onChange={e => {this.setState({ term: e.target.value })}}></input>
+                        onChange={e => {this.setState({ term: e.target.value })}}
+                        placeholder="Search here..."
+                    ></input>
                     <div className="help">
                         <p className="helpTextHeading">Example searches:</p>
-                        <p className="helpText">"Manchester GB" - Includes country code.</p>
+                        <p className="helpText">"Manchester GB" - Includes country code. **MOST ACCURATE**</p>
                         <p className="helpText">"Manchester" - No country code.</p>
                         <p className="helpText">Find country code abbreviations <a href="https://abbreviations.yourdictionary.com/articles/country-abbreviations.html" rel="noopener noreferrer" target="_blank">here</a>.</p>
                     </div>
